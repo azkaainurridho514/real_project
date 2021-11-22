ee30ef9883bf5a4e8c0df2074603bb14a02845c7
azkaainurridho514 <azkaainurridho514@gmail.com>
2021-11-22 10:39:58 +0700

    memperbaiki beberapa fitur

--- real_project.sql
+++ real_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Nov 2021 pada 04.30
+-- Waktu pembuatan: 22 Nov 2021 pada 04.39
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 8.0.12
 
